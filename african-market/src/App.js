01b33a2111c3e7de1a74ae43003c3a7521ac779c
Garrett Yien <84867674+garrettwyien@users.noreply.github.com>
2021-08-24 13:45:50 -0400

    added placeholders for header and home components and added new files to the component list to be fleshed out

--- african-market/src/App.js
+++ african-market/src/App.js
@@ -1,7 +1,11 @@
 import React, {useState} from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import ItemForm from './Components/ItemForm';
-import { Route, Switch } from 'react-router-dom';
+// import Header from './Components/Header';
+// import Login from './Components/Login';
+// import Home from './Components/Home';
+
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -11,17 +15,21 @@ function App() {
 
   return (
     <div className="App">
-      {/* HEADER GOES HERE
-      <Header/> */}
+      {/* HEADER GOES HERE */}
+      {/* <Header/> */}
 
       <Switch>
         <Route path='/itemform'>
           <ItemForm />
         </Route>
-        {/* LOGIN COMPONENT GOES HERE 
-        <Route path='/login'>
+        {/* LOGIN COMPONENT GOES HERE  */}
+        {/* <Route path='/login'>
           <Login login={login}/>
         </Route> */}
+        {/* HOME COMPONENT GOES HERE */}
+        {/* <Route exact path='/'>
+          <Home/>
+        </Route>  */}
       </Switch>
       
     </div>
