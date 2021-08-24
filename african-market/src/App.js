170cd722d6ecf7743352a714bf81c541e3b9a4a8
Garrett Yien <84867674+garrettwyien@users.noreply.github.com>
2021-08-24 13:34:28 -0400

    added state and placeholders for login and placeholder for header, added switches to app.js

--- african-market/src/App.js
+++ african-market/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react';
 import './App.css';
-import ItemForm from './Components/ItemForm'
+import ItemForm from './Components/ItemForm';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const login=()=>{
+    setLoggedIn(true);
+  };
+
   return (
     <div className="App">
-      <ItemForm />
+      {/* HEADER GOES HERE
+      <Header/> */}
+
+      <Switch>
+        <Route path='/itemform'>
+          <ItemForm />
+        </Route>
+        {/* LOGIN COMPONENT GOES HERE 
+        <Route path='/login'>
+          <Login login={login}/>
+        </Route> */}
+      </Switch>
+      
     </div>
   );
-}
+};
 
 export default App;
