c66e1a1e9bc2e45bddf0510d0bb9c9215417206c
Garrett Yien <84867674+garrettwyien@users.noreply.github.com>
2021-08-25 16:21:21 -0400

    styled header a bit

--- african-market/src/Components/Header.js
+++ african-market/src/Components/Header.js
@@ -1,26 +1,77 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from "styled-components";
 
 const Header=(props)=>{
     const { loggedIn } = props;
     
     return (
-        <header>
+        <>
+        {!loggedIn && 
+        <StyledHeaderLoggedOut>
             <h2>African Marketplace</h2>
             <nav>
                 <Link to='/'>Home</Link>
-                {loggedIn &&
-                <Link to='/itemlist'>Marketplace</Link>}
-                {loggedIn &&
-                <Link to='/itemform'>Add Item</Link>}
-                {!loggedIn &&
-                <Link to='/login'>Login</Link>}
-                {!loggedIn &&
-                <Link to='/signup'>Sign up</Link>}     
-                {loggedIn &&
-                <Link to='/logout'>Logout</Link>}
+                <Link to='/login'>Login</Link>
+                <Link to='/signup'>Sign up</Link>    
             </nav>
-        </header>
+        </StyledHeaderLoggedOut>}
+        {loggedIn &&
+        <StyledHeaderLoggedIn>
+            <h2>African Marketplace</h2>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/itemlist'>Marketplace</Link>
+                <Link to='/itemform'>Add Item</Link>
+                <Link to='/logout'>Logout</Link>
+            </nav>
+        </StyledHeaderLoggedIn>}
+        </>
+
+        
     );
 };
 export default Header;
+
+
+const StyledHeaderLoggedOut = styled.header`
+display:flex;
+justify-content: space-between;
+align-items:center;
+
+h2 {
+    padding-left: 3%;
+}
+
+nav {
+    display:flex;
+    width: 25%;
+    justify-content: space-between;
+    margin-right: 3%;
+}
+a {
+    text-decoration: none;
+  }
+
+`
+const StyledHeaderLoggedIn = styled.header`
+display:flex;
+justify-content: space-between;
+align-items:center;
+
+h2 {
+    padding-left: 3%;
+}
+
+nav {
+    display:flex;
+    width: 40%;
+    justify-content: space-between;
+    margin-right: 3%;
+}
+a {
+    text-decoration: none;
+  }
+
+`
+
