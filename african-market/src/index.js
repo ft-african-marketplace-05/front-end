a659cb4c86ad241778ab8f031b73ffe0ff10079f
Garrett Yien <84867674+garrettwyien@users.noreply.github.com>
2021-08-25 17:24:55 -0400

    implemented jason's styling

--- african-market/src/index.js
+++ african-market/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/ItemForm.css'
+import './Components/ItemForm.css'
 
 ReactDOM.render(
     <Router>
